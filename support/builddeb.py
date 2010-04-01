6a79194e3407955c809bc8ea8d1ffe0cd55755da
Ed Page <eopage@byu.net>
2010-04-01 14:19:20 -0500

    Bump to 0.3.8-1

--- support/builddeb.py
+++ support/builddeb.py
@@ -21,6 +21,9 @@ __email__ = "n800@axique.net"
 __version__ = constants.__version__
 __build__ = constants.__build__
 __changelog__ = """
+0.3.8
+* Fixed check boxes on Maemo
+
 0.3.7
 * Corrected the bug tracker link
 
